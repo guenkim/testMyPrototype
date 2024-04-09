b60ed5e12f320eedc0add9d3ceca64de3bf5a036
guenkim <kkaddus@naver.com>
2024-04-09 11:10:49 +0900

    Update BackendApplication.java

--- backend/src/main/java/com/guen/BackendApplication.java
+++ backend/src/main/java/com/guen/BackendApplication.java
@@ -11,6 +11,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("line-서버수정");
+		System.out.println("line-서버추가");
 	}
 
 }
