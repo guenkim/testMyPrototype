9a98ae47f115c692c0720df7852a4c676cbcbd09
guenkim <kkaddus@naver.com>
2024-04-09 13:19:25 +0900

    Update BackendApplication.java

--- backend/src/main/java/com/guen/BackendApplication.java
+++ backend/src/main/java/com/guen/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("서버수정");
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("서버수정");
 	}
